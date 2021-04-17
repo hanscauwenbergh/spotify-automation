25053fa130c475f619068adfc2d82a5341fcf1cd
Hans Cauwenbergh <hans@grid.gg>
2021-04-17 20:48:43 +0200

    remove unused files

--- src/main/kotlin/com/hanscauwenbergh/Application.kt
+++ src/main/kotlin/com/hanscauwenbergh/Application.kt
@@ -3,13 +3,11 @@ package com.hanscauwenbergh
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import com.hanscauwenbergh.commands.GetTokensCommand
-import com.hanscauwenbergh.commands.RefreshPlaylistsCommand
 import com.hanscauwenbergh.commands.RefreshRecentSelectionCommand
 
 fun main(vararg args: String) = SpotifyAutomation()
     .subcommands(
         GetTokensCommand(),
-        RefreshPlaylistsCommand(),
         RefreshRecentSelectionCommand()
     )
     .main(args)
